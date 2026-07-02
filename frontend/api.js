cbb948d3b546a22b14b9d3925aa20fdbbb954dd0
hyunsuprice <pricehyunsu@gmail.com>
2026-07-02 14:54:16 +0200

    setup

--- frontend/api.js
+++ frontend/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:3000";
+const url = "http://http://localhost:5173/api";
 
 export default async function postData(collection, formData) {
   try {
